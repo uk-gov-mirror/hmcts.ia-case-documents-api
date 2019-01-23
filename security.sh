6cc8bf840b621789639c8d3d6b5a046c6b7b2601
Rob Doherty <robdohertykelly@gmail.com>
2019-01-23 11:27:28 +0000

    testing exclusion of swagger endpoint

--- security.sh
+++ security.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+echo 'testing create file' > test_create.txt
+cat test_create.txt
 echo '{
   "site" : "https://ia-case-documents-api-aat.service.core-compute-aat.internal/v2/api-docs",
   "issues" : [
@@ -11,6 +13,7 @@ echo '{
     }
   ]
 }%' > security-ignore.conf
+cat security-ignore.conf
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -p security-ignore.conf
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
