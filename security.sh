e55bb18d61c8f965ac149e13633c8dcfa98d1d4c
Rob Doherty <robdohertykelly@gmail.com>
2019-01-23 11:19:47 +0000

    testing exclusion of swagger endpoint

--- security.sh
+++ security.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+echo '{
+  "site" : "https://ia-case-documents-api-aat.service.core-compute-aat.internal/v2/api-docs",
+  "issues" : [
+    {
+      "id" : "90022",
+      "name" : "Application Error Disclosure",
+      "state" : "inprogress",
+      "link": "https://tools.hmcts.net/jira/browse/RIA-989"
+    }
+  ]
+}%' > security-ignore.conf
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -P 1001 -p security-ignore.conf
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
